50224f2a8c468d723337c2b00c2a5d4645d372bc
jianghe <573748150jh@163.com>
2018-04-08 13:50:40 +0800

    update

--- testreactdemo/src/App.js
+++ testreactdemo/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, WingBlank, Range } from 'antd-mobile';
 
 class App extends Component {
+
   render() {
+    const log = (name) => {
+      return (value) => {
+        console.log(`${name}: ${value}`);
+      };
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WingBlank size="lg">
+          <p className="sub-title">Basic range</p>
+          <Range
+            style={{ marginLeft: 30, marginRight: 30 }}
+            min={0}
+            max={20}
+            defaultValue={[3, 10]}
+            onChange={log('change')}
+            onAfterChange={log('afterChange')}
+          />
+        </WingBlank>
       </div>
     );
   }
